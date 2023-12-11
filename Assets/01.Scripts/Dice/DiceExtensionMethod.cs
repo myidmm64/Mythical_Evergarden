8997519ef035fc9581827ff870b9c160ee335997
Jae Yeop Lee <myidmm64@gmail.com>
2023-12-11 22:14:05 +0900

    1211_dices 내 IDiceUnit 탐색

--- Assets/01.Scripts/Dice/DiceExtensionMethod.cs
+++ Assets/01.Scripts/Dice/DiceExtensionMethod.cs
@@ -6,6 +6,25 @@ using UnityEngine;
 public static class DiceExtensionMethod
 {
     /// <summary>
+    /// IEnumerable 내의 IDiceUnit들을 반환합니다.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="dices"></param>
+    /// <returns></returns>
+    public static IEnumerable<IDiceUnit> GetIDiceUnits(this IEnumerable<Dice> dices)
+    {
+        List<IDiceUnit> result = new List<IDiceUnit>();
+        foreach (var dice in dices)
+        {
+            if(dice.diceUnit != null)
+            {
+                result.Add(dice.diceUnit);
+            }    
+        }
+        return result;
+    }
+
+    /// <summary>
     /// IEnumerable 내의 중복되는 Dice들을 하나만 남기고 없애줍니다.
     /// </summary>
     /// <param name="dices"></param>
